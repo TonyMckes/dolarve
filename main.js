0bd91d9a3fc25701a6987e9d7bc850f870febde1
TonyMckes <tonymckes@gmail.com>
2021-10-14 17:19:11 -0400

    Several changes, I always forget to do commints(?

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
-const url = "https://s3.amazonaws.com/dolartoday/data.json";
+// const url = "https://s3.amazonaws.com/dolartoday/data.json";
+const vcoudUrl = "https://exchange.vcoud.com/coins/";
 
-fetchData(url);
+fetchData(vcoudUrl);
 
 async function fetchData(url) {
+  //* call loading animation
   try {
     const response = await fetch(url);
     const data = await response.json();
-    renderData(data);
+    processData(data);
+    //* remove loading animation
   } catch (error) {
     console.warn(error);
   }
 }
 
-function renderData(data) {
-  const card = document.querySelector(".container");
+function processData(jsonData) {
+  const raw = jsonData;
+  const filterIn = [0, 1, 2, 4, 6, 7, 9, 13, 19];
+  // const filterOut = [3, 5, 8, 10, 11, 12]; //
+  const arr = raw.filter((arg, index) => filterIn.includes(index));
+  // arr.length = 9
+  drawElements(arr);
+}
 
-  for (const key in data.USD) {
-    // console.log(`${key} = ${data.USD[key]}`);
-    const itemCard = document.createElement("div");
-    itemCard.className = "item-card";
-    card.append(itemCard);
+function drawElements(arr) {
+  const container = document.querySelector(".container");
 
-    const itemList = document.createElement("li");
-    itemList.className = "actual-value";
-    itemList.textContent = data.USD[key];
-    itemCard.append(itemList);
-  }
+  arr.forEach((element) => {
+    const itemList = document.createElement("div");
+
+    // ? Esto se puede mejorar;
+    //TODO:
+    let name = element.name;
+    let price = element.price;
+    let priceOld = element.priceOld;
+    let updatedAt = element.updatedAt;
+    let slug = element.slug;
+    let icon = element.icon;
+
+    itemList.className = `item-card ${slug}`;
+    itemList.innerText = name;
+    container.append(itemList);
+  });
+
+  const elementArr = ["h6", "img", "span", "span", "p"];
+  const itemDiv = document.querySelectorAll(".item-card");
+  itemDiv.append(...elementArr);
 }
