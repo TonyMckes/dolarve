bc1273549fa0aa73049d79dada7becf39d8bd96d
TonyMckes <tonymckes@gmail.com>
2021-10-21 17:37:58 -0400

    Change how dates/times were displayed, also I moved calc() to the main renderElements()

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // const url = "https://s3.amazonaws.com/dolartoday/data.json";
-const vcoudUrl = "https://exchange.vcoud.com/coins/";
+const vcoud = "https://exchange.vcoud.com/coins/";
 const offline = "offline.json";
 
+// Currency formatter
 const formatter = new Intl.NumberFormat("es-VE", {
   style: "currency",
   currency: "VES",
 });
 
-const loadingWindow = document.querySelector("#loading-window");
-const loading = document.querySelector("#loading");
+const fadeBackground = document.querySelector("#loading-window");
+const loadingSpinner = document.querySelector("#loading");
 
 document.querySelector("button").addEventListener("click", refresh);
 
@@ -16,100 +17,137 @@ function refresh() {
   // Show loading animation
   showLoading();
 
-  fetchData(vcoudUrl);
+  fetchData(vcoud);
 }
 
-fetchData(vcoudUrl);
+fetchData(vcoud);
 
+// Get info from server
 async function fetchData(url) {
-  //  Show loading animation
-  showLoading();
-
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-    processData(data);
+    //  Show loading animation while waiting for a response
+    showLoading();
 
-    // Hide loading animation
-    loadingWindow.classList.remove("display");
-    loading.classList.remove("display");
+    // Wait for response
+    const response = await fetch(url);
+
+    // If response is OK, take json and return an array with objects
+    const dataArray = await response.json();
+
+    // Then call
+    processData(dataArray);
+
+    // After the page gets rendered, hide loading indicators
+    fadeBackground.classList.remove("display");
+    loadingSpinner.classList.remove("display");
+
+    //Then
   } catch (error) {
+    // If something goes wrong then;
     // TODO: Add an alert or option to reload
     console.warn(error);
   }
 }
-// Show loading animation
+
 function showLoading() {
-  loadingWindow.classList.add("display");
-  loading.classList.add("display");
+  fadeBackground.classList.add("display");
+  loadingSpinner.classList.add("display");
 }
 
-function processData(data) {
-  // Filter: selected ones only
-  const filterElem = [0, 1, 2, 4, 6, 7, 9]; //13, 19
+//
+function processData(dataArray) {
+  // Filter selected ones only
+  const selection = [0, 1, 2, 4, 6, 7, 9]; //13, 19
+  const currencies = dataArray.filter((_, i) => selection.includes(i));
 
-  // Filter process
-  const dataInfo = data.filter((arg, index) => filterElem.includes(index));
+  // Select container in html
+  const container = document.querySelector(".container");
 
   // Select input and add event
   const input = document.querySelector("#input-box");
-  input.addEventListener("input", calc);
 
-  // Select container in html
-  const table = document.querySelector(".container");
+  // input.addEventListener("input", calc);
+  input.addEventListener("input", renderElements);
 
-  // Check if ".container" doesn't have any child elements
-  table.firstChild ? calc() : drawElements();
+  // Start displaying elements
+  renderElements();
 
   // Set "#input-box" to display
   input.style.display = "inline-block";
 
-  //
-  function calc() {
-    // Select all classes
-    const prices = document.querySelectorAll(".calcValue");
-    const dates = document.querySelectorAll(".dateTime");
+  // Calculate time since last data was fetched
+  function timeAgo(input) {
+    const date = input instanceof Date ? input : new Date(input);
 
-    // For every class, update only the innerText
-    for (let i = 0; i < dataInfo.length; i++) {
-      prices[i].innerText = formatter.format(dataInfo[i].price * input.value);
-      dates[i].innerText = new Date(dataInfo[i].updatedAt).toLocaleString(
-        "es-VE"
-      );
+    const formatter = new Intl.RelativeTimeFormat("es");
+
+    const ranges = {
+      years: 3600 * 24 * 365,
+      months: 3600 * 24 * 30,
+      weeks: 3600 * 24 * 7,
+      days: 3600 * 24,
+      hours: 3600,
+      minutes: 60,
+      seconds: 1,
+    };
+
+    const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+
+    for (let key in ranges) {
+      if (ranges[key] < Math.abs(secondsElapsed)) {
+        const delta = secondsElapsed / ranges[key];
+        return formatter.format(Math.round(delta), key);
+      }
     }
   }
 
-  // This function only runs once
-  function drawElements() {
-    // Loop over dataInfo(array) and call the function each time
-    dataInfo.forEach((obj) => {
-      // Create HTML elements
-      const tableRow = document.createElement("section");
-      const tableImg = document.createElement("img");
-      const tableName = document.createElement("h3");
-      const tablePrice = document.createElement("p");
-      const tableDate = document.createElement("p");
+  // Start displaying elements
+  function renderElements() {
+    // If the container doesn't have any element child's, then
+    if (!container.firstChild) {
+      // Start creating elements
+      currencies.forEach((currency) => {
+        // Create HTML elements
+        const section = document.createElement("section");
+        const coinImg = document.createElement("img");
+        const coinName = document.createElement("h3");
+        const coinValue = document.createElement("p");
+        const coinDate = document.createElement("p");
 
-      tableRow.className = `item-card ${obj.slug}`;
+        section.className = `item-card ${currency.slug}`;
 
-      // Process image
-      tableImg.src = obj.icon;
-      tableImg.alt = obj.name;
+        // Process image
+        coinImg.src = currency.icon;
+        coinImg.alt = currency.name;
 
-      // Process name
-      tableName.innerText = obj.name;
+        // Process name
+        coinName.innerText = currency.name;
 
-      // Process currency value
-      tablePrice.innerText = formatter.format(obj.price); //* Update this text for each element.
-      tablePrice.className = "calcValue";
+        // Process currency value
+        coinValue.className = "coinValue";
+        coinValue.innerText = formatter.format(currency.price);
 
-      // Process dates
-      tableDate.innerText = new Date(obj.updatedAt).toLocaleString("es-VE");
-      tableDate.className = "dateTime";
+        // Process dates
+        // coinDate.innerText = new Date(obj.updatedAt).toLocaleString("es-VE");
+        coinDate.className = "coinDate";
+        coinDate.innerText = timeAgo(currency.updatedAt);
 
-      // Append to DOM
-      table.appendChild(tableRow);
-      tableRow.append(tableImg, tableName, tablePrice, tableDate);
-    });
+        // Append to DOM
+        container.appendChild(section);
+        section.append(coinImg, coinName, coinValue, coinDate);
+      });
+
+      // Update existing elements
+    } else {
+      // Select classes
+      const values = document.querySelectorAll(".coinValue");
+      const dates = document.querySelectorAll(".coinDate");
+
+      currencies.forEach((currency, i) => {
+        // Update existing classes with up to date information
+        values[i].innerText = formatter.format(currency.price * input.value);
+        dates[i].innerText = timeAgo(currency.updatedAt);
+      });
+    }
   }
 }
