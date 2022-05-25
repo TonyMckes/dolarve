c3beb646a98df64fe2a5cda69179c21c3fbe8086
TonyMckes <tonymckes@gmail.com>
2022-05-25 13:19:15 -0400

    Added disabled status indicator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
-function Button({ icon, onClick, text }) {
+function Button({ disabled, icon, onClick, text }) {
   return (
     <button
-      className="px-2 py-1 font-medium rounded-xl hover:bg-neutral-100 dark:hover:bg-neutral-700 ring-1 ring-neutral-450 "
+      className="px-2 py-1 font-medium text-left disabled:cursor-not-allowed disabled:opacity-50 rounded-xl hover:bg-neutral-100 dark:hover:bg-neutral-700 ring-1 ring-neutral-450 "
+      disabled={disabled}
       onClick={onClick}
     >
       {icon} <span className="">{text}</span>
