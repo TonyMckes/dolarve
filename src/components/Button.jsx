f116eba7b7b632d1271958f3c3a112d4e2212785
TonyMckes <tonymckes@gmail.com>
2022-05-25 13:29:38 -0400

    Fixed colors on hover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 function Button({ disabled, icon, onClick, text }) {
   return (
     <button
-      className="px-2 py-1 font-medium text-left disabled:cursor-not-allowed disabled:opacity-50 rounded-xl hover:bg-neutral-100 dark:hover:bg-neutral-700 ring-1 ring-neutral-450 "
+      className="px-2 py-1 font-medium text-left disabled:cursor-not-allowed disabled:opacity-50 rounded-xl hover:bg-neutral-200 dark:hover:bg-neutral-700 ring-1 ring-neutral-450 "
       disabled={disabled}
       onClick={onClick}
     >
